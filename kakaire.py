d54a93cb1bead224ea746f4d3d9ead4382b10628
kakairesteven <kakairestevenking@gmail.com>
2017-05-17 15:25:11 +0300

    Update kakaire.py

--- kakaire.py
+++ kakaire.py
@@ -1,14 +1,3 @@
-Python 2.6.5 (r265:79096, Mar 19 2010, 21:48:26) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
-
-    ****************************************************************
-    Personal firewall software may warn about the connection IDLE
-    makes to its subprocess using this computer's internal loopback
-    interface.  This connection is not visible on any external
-    interface and no data is sent to or received from the Internet.
-    ****************************************************************
-    
-IDLE 2.6.5      
 >>> # 1 - Import library
 import pygame
 from pygame.locals import*
